0057a116ba6a832f2ef90da52a93833617b2d789
Norbert Pabisz <n.pabisz@sintraconsulting.eu>
2022-06-27 16:26:03 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,286 @@
+'use strict';
+
+const { src, dest, watch, parallel, series, task } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer');
+const rename = require('gulp-rename');
+const foreach = require('gulp-foreach');
+const babel = require('gulp-babel');
+const plumber = require('gulp-plumber');
+const minify = require('gulp-minify');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const path = require('path');
+
+class Gulp
+{
+  constructor (_exports, config)
+  {
+    task('js', this.taskJs.bind(this));
+    task('css', this.taskCss.bind(this));
+
+    task('vendor-css', this.taskVendorCss.bind(this));
+    task('vendor-js', this.taskVendorJs.bind(this));
+
+    task('watch', this.taskWatcher.bind(this));
+
+    _exports.default = this.taskDefault();
+
+    this.config = this.deepMerge(this.defaults(), config ? config : {});
+  }
+
+  defaults ()
+  {
+    return {
+      prefix: 'custom',
+      glue: '.',
+      dest: '../assets',
+      split: true,
+      css: {
+        src: 'sass/**/*',
+        name: 'style',
+        vendors: 'vendors/css/*.css',
+        vendorsName: 'vendors',
+        baseDirectory: 'sass'
+      },
+      js: {
+        src: 'js/**/*',
+        name: 'all',
+        vendors: 'vendors/js/*.js',
+        vendorsName: 'vendors',
+        uglify: true,
+        onlyMinified: true,
+        babelOptions: {
+          presets: [
+            ['@babel/env', {
+              modules: false
+            }]
+          ],
+          plugins: [
+            ['@babel/plugin-transform-classes', {
+              loose: false
+            }]
+          ]
+        },
+        baseDirectory: 'js'
+      }
+    }
+  }
+
+  isObject (item)
+  {
+    return (item && typeof item === 'object' && !Array.isArray(item));
+  }
+
+  deepMerge (target, ...sources)
+  {
+    if (!sources.length) {
+      return target;
+    }
+
+    const source = sources.shift();
+
+    if (this.isObject(target) && this.isObject(source)) {
+      for (const key in source) {
+        if (this.isObject(source[key])) {
+          if (!target[key]) {
+            Object.assign(target, { [key]: {} });
+          }
+          this.deepMerge(target[key], source[key]);
+        } else {
+          Object.assign(target, { [key]: source[key] });
+        }
+      }
+    }
+
+    return this.deepMerge(target, ...sources);
+  }
+
+  taskJs ()
+  {
+    const config = this.config;
+
+    if (!config.split) {
+      let result = src(config.js.src)
+        .pipe(plumber())
+        .pipe(babel(config.js.babelOptions))
+        .pipe(concat(config.js.name + '.js'));
+
+      if (!config.js.onlyMinified) {
+        result = result.pipe(dest(config.dest));
+      }
+
+      result = result.pipe(rename(config.js.name + '.min.js'));
+
+      if (config.js.uglify) {
+        result = result.pipe(uglify());
+      } else {
+        result = result.pipe(minify());
+      }
+
+      result = result.pipe(dest(config.dest));
+
+      return result;
+    }
+
+    return src(config.js.src)
+      .pipe(foreach(function (stream, file) {
+        if (file.isDirectory()) {
+          return stream;
+        }
+
+        const name = path.basename(file.history[0]);
+        const dirs = [];
+        let type = '';
+        let dir = path.dirname(file.history[0]);
+
+        while (true) {
+          type = path.basename(dir);
+          dir = path.dirname(dir);
+
+          if (type === config.js.baseDirectory) {
+            break;
+          } else if (type === '') {
+            throw 'Invalid directory structure, missing ' + config.js.baseDirectory;
+          }
+
+          dirs.push(type);
+        }
+
+        let renamed = name;
+
+        if (dirs.length > 0) {
+          renamed = dirs.reverse().join(config.glue) + config.glue + renamed;
+        }
+
+        if (config.prefix) {
+          renamed = config.prefix + config.glue + renamed;
+        }
+
+        let result = src(file.history[0])
+          .pipe(plumber())
+          .pipe(babel(config.js.babelOptions));
+
+        if (!config.js.onlyMinified) {
+          result = result.pipe(rename(renamed))
+            .pipe(dest(config.dest));
+        }
+
+        result = result.pipe(rename(renamed.replace('.js', '.min.js')));
+
+        if (config.js.uglify) {
+          result = result.pipe(uglify());
+        } else {
+          result = result.pipe(minify());
+        }
+
+        result = result.pipe(dest(config.dest));
+
+        return result;
+      }));
+  }
+
+  taskCss ()
+  {
+    const config = this.config;
+
+    if (!config.split) {
+      return src(config.css.src)
+        .pipe(sass({
+          outputStyle: 'compressed'
+        }))
+        .pipe(autoprefixer())
+        .pipe(rename(config.css.name + '.min.css'))
+        .pipe(dest(config.dest));
+    }
+
+    return src(config.css.src)
+      .pipe(sass().on('error', sass.logError))
+      .pipe(foreach(function (stream, file) {
+        if (file.isDirectory()) {
+          return stream;
+        }
+
+        const name = path.basename(file.history[0]);
+        const dirs = [];
+        let type = '';
+        let dir = path.dirname(file.history[0]);
+
+        while (true) {
+          type = path.basename(dir);
+          dir = path.dirname(dir);
+
+          if (type === config.css.baseDirectory) {
+            break;
+          } else if (type === '') {
+            throw 'Invalid directory structure, missing ' + config.css.baseDirectory;
+          }
+
+          dirs.push(type);
+        }
+
+        let renamed = name.replace('.scss', '.min.css');
+
+        if (dirs.length > 0) {
+          renamed = dirs.reverse().join(config.glue) + config.glue + renamed;
+        }
+
+        if (config.prefix) {
+          renamed = config.prefix + config.glue + renamed;
+        }
+
+        return src(file.history[0])
+          .pipe(sass({
+            outputStyle: 'compressed'
+          }))
+          .pipe(autoprefixer())
+          .pipe(rename(renamed))
+          .pipe(dest(config.dest));
+      }));
+  }
+
+  taskVendorJs ()
+  {
+    let name = this.config.js.vendorsName + '.min.js';
+
+    if (this.config.prefix) {
+      name = this.config.prefix + this.config.glue + name;
+    }
+
+    return src(this.config.js.vendors)
+      .pipe(concat(name))
+      .pipe(dest(this.config.dest));
+  }
+
+  taskVendorCss ()
+  {
+    let name = this.config.css.vendorsName + '.min.css';
+
+    if (this.config.prefix) {
+      name = this.config.prefix + this.config.glue + name;
+    }
+
+    return src(this.config.css.vendors)
+      .pipe(concat(name))
+      .pipe(dest(this.config.dest));
+  }
+
+  taskWatcher ()
+  {
+    watch(this.config.css.src, parallel('css'));
+    watch(this.config.js.src, parallel('js'));
+    watch(this.config.css.vendors, parallel('vendor-css'));
+    watch(this.config.js.vendors, parallel('vendor-js'));
+  }
+
+  taskDefault ()
+  {
+    return series(
+      parallel('vendor-css', 'vendor-js'),
+      parallel('css', 'js'),
+      'watch'
+    );
+  }
+}
+
+module.exports = { Gulp };
---
+++ readme.md
@@ -0,0 +1,121 @@
+# Gulp for Sintra
+
+This is pre-made gulp tasks for shopify projects.
+
+## Basic usage
+
+Minimal `Gulpfile.js` with default configuration:
+
+```js
+const { Gulp } = require('./gulp-sintra/index');
+
+new Gulp(exports);
+```
+
+Additional configuration:
+
+```js
+const { Gulp } = require('./gulp-sintra/index');
+
+new Gulp(exports, {
+  prefix: 'new-project'  
+});
+```
+
+## Tasks
+
+* `default` task will merge vendors, compile js & css and then watch for changes
+* `vendor-js` task will merge vendor js
+* `vendor-css` task will merge vendor css
+* `js` task will compile source javascript
+* `css` task will compile source scss
+
+## Structure
+
+Default `Dawn` theme project structure should be as follows:
+
+    .
+    ├── dev                         
+    │   ├── js                      
+    │   │   ├── sections 
+    │   │   │   └── example.js      # Each section should have very own js
+    │   │   └── core.js             # Global js used site-wide
+    │   ├── sass    
+    │   │   ├── components 
+    │   │   │   └── example.scss    # Components like eg. rating
+    │   │   ├── core 
+    │   │   │   └── _vars.scss      # Css variables
+    │   │   ├── sections 
+    │   │   │   └── example.scss    # Each section should have very own css
+    │   │   ├── _mixins.scss        # Common functions and mixins
+    │   │   ├── _variables.scss     # Sass variables
+    │   │   └── core.scss           # Core styles used site-wide
+    │   ├── vendors  
+    │   │   ├── js      
+    │   │   │   └── example.min.js  # Vendor js files which will be merged into one
+    │   │   └── css      
+    │   │       └── example.min.css # Vendor css files which will be merged into one
+    │   ├── Gulpfile.js
+    │   ├── package.json
+    │   └── package-lock.json
+    └── ...
+
+## Configuration
+
+This is default configuration:
+
+```js
+new Gulp(exports, {
+  // Destination directory for compiled files 
+  dest: '../assets',
+  // Prefix for compiled files
+  prefix: 'custom',
+  // Character for joining directory structured files
+  glue: '.',
+  // Split each source file to css file
+  split: true,
+  // Css config
+  css: {
+    // Source path for sass files
+    src: 'sass/**/*',
+    // Used for single output file (if split set to false)
+    name: 'style',
+    // Vendors source path      
+    vendors: 'vendors/css/*.css',
+    // Vendors output file name
+    vendorsName: 'vendors',
+    // Base directory where source files are stored
+    baseDirectory: 'sass'
+  },
+  // Js config
+  js: {
+    // Source path for js files
+    src: 'js/**/*',
+    // Used for single output file (if split set to false)
+    name: 'all',
+    // Vendors source path      
+    vendors: 'vendors/js/*.js',
+    // Vendors output file name
+    vendorsName: 'vendors',
+    // Uglify output file
+    uglify: true,
+    // This prevent clean js from rendering to file
+    onlyMinified: true,
+    // Base directory where source js files are stored
+    baseDirectory: 'js',
+    // Options for babel compiler
+    babelOptions: {
+      presets: [
+        ['@babel/env', {
+          modules: false
+        }]
+      ],
+      plugins: [
+        ['@babel/plugin-transform-classes', {
+          loose: false
+        }]
+      ]
+    }
+  }
+})
+```
